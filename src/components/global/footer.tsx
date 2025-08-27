4187a6a17bd403295f1beb16733cd3cc8981e62f
yuvrajjwala-mmg <meetyuvrajjwala@gmail.com>
2025-08-27 16:01:02 +0530

    chore: footer line animation

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -3,15 +3,57 @@
 import { SOCIALS } from '@/data/socials';
 import { Icon } from '@iconify/react';
 import Link from 'next/link';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useScrambleOnEnter } from '../../hooks/useScrambleOnEnter';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Footer = () => {
   const footerRef = useRef<HTMLElement | null>(null);
   const line1Ref = useRef<HTMLDivElement | null>(null);
   const line2Ref = useRef<HTMLDivElement | null>(null);
+  const leftLineRef = useRef<HTMLDivElement | null>(null);
+  const rightLineRef = useRef<HTMLDivElement | null>(null);
 
   useScrambleOnEnter(footerRef, [line1Ref, line2Ref]);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    if (!footerRef.current) return;
+
+    const ctx = gsap.context(() => {
+      if (leftLineRef.current && rightLineRef.current) {
+        gsap.set(leftLineRef.current, { width: 0 });
+        gsap.set(rightLineRef.current, { width: 0 });
+
+        gsap.to(leftLineRef.current, {
+          width: '100%',
+          ease: 'none',
+          scrollTrigger: {
+            trigger: leftLineRef.current,
+            start: 'top bottom',
+            end: '+=400',
+            scrub: 0.5,
+          },
+        });
+
+        gsap.to(rightLineRef.current, {
+          width: '100%',
+          ease: 'none',
+          scrollTrigger: {
+            trigger: rightLineRef.current,
+            start: 'top bottom',
+            end: '+=400',
+            scrub: 0.5,
+          },
+        });
+      }
+    }, footerRef);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <footer
       ref={footerRef}
@@ -23,12 +65,18 @@ const Footer = () => {
           <div ref={line2Ref}>DOES NOT DIE</div>
         </div>
         <div className='flex justify-center items-center gap-3'>
-          <div className='h-[2px] flex-1 bg-[#9CC1C1]' />
+          <div
+            ref={leftLineRef}
+            className='h-[2px] bg-[#9CC1C1] w-0'
+          />
           <Icon
             icon='streamline-sharp:star-2-solid'
-            className='text-[#D9D9D9] text-[20px]'
+            className='text-[#D9D9D9] text-[60px]'
           />
-          <div className='h-[2px] flex-1 bg-[#9CC1C1]' />
+          <div
+            ref={rightLineRef}
+            className='h-[2px] bg-[#9CC1C1] w-0'
+          />
         </div>
         <div className='flex justify-center items-center gap-6'>
           {SOCIALS.map((social) => (
